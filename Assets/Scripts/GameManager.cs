757d07475cbcd485795734ce5a206b2518262582
Isaac-Erickson <144268763+Isaac-Erickson@users.noreply.github.com>
2024-03-18 10:29:33 -0700

    Mar. 18, 2024
    Added  opponent deck dealing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,18 +7,20 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
     public List<Card> deck = new List<Card>();
+
     public List<Card> player_deck = new List<Card>();
+
     //public List<Card> ai_deck = new List<Card>();
     public List<Card> player_hand = new List<Card>();
-    //public List<Card> ai_hand = new List<Card>();
+    public List<Card> ai_hand = new List<Card>();
     public List<Card> discard_pile = new List<Card>();
     //public List<Transform> card_positon = new List<Transform>();
-    
+
     public float offset;
-    
+
     public Transform _canvas;
-    
-    
+
+
     private void Awake()
     {
         if (gm != null && gm != this)
@@ -31,50 +33,71 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
+        DealOpponent();
         Deal();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Deal()
     {
 
-        int dealSize = 1;
-        
+        int dealSize = 4;
+
         for (int i = 0; i < dealSize; i++)
         {
-            int randomCard = Random.Range(0, deck.Count);
-            
-            Card randomDeckCard = Instantiate(deck[randomCard], new Vector3(transform.position.x + offset, 200, 0), Quaternion.identity);
+            int randomCard = Random.Range(0, player_deck.Count);
+
+            Card randomDeckCard = Instantiate(player_deck[randomCard],
+                new Vector3(transform.position.x + offset, 200, 0), Quaternion.identity);
             player_hand.Add(randomDeckCard);
             randomDeckCard.transform.SetParent(_canvas);
-            deck.RemoveAt(randomCard);
+            player_deck.RemoveAt(randomCard);
             offset += 200;
             //May need to be Remove or RemoveAt
-            
+
             //Use "Canvas" for parent
             //for card_position try an offset instead, so a list of positions isn't needed
         }
     }
 
-    void Shuffle()
+    void DealOpponent()
     {
-        
+        int dealSize = 1;
+
+        for (int i = 0; i < dealSize; i++)
+        {
+            int randomCard = Random.Range(0, deck.Count);
+
+            Card randomDeckCard = Instantiate(deck[randomCard], new Vector3(transform.position.x + offset, 600, 0),
+                Quaternion.identity);
+            ai_hand.Add(randomDeckCard);
+            randomDeckCard.transform.SetParent(_canvas);
+            deck.RemoveAt(randomCard);
+        }
+
+
+        void Shuffle()
+        {
+
+        }
+
+        void AI_Turn()
+        {
+
+        }
+
+
+
+
     }
-
-    void AI_Turn()
-    {
-
-    }
-
-
-
-    
 }
